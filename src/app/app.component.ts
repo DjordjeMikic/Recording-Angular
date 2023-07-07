d5cb28af4a266bd788bcfc9473938234b30c8e8e
djole <mikic@usertechnologies.com>
2023-07-07 04:26:20 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ScreenRecording } from 'screen-recording';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
+})
+export class AppComponent {
+  title: string = 'Recording App';
+  interval: ReturnType<typeof setInterval> | null = null;
+  blinking: boolean = false;
+ 
+  async downloadRecording(blob: Blob) {
+    try {
+      const newHandle = await (window as any).showSaveFilePicker({
+        suggestedName: 'recorded.webm',
+      });
+      const writableStream = await newHandle.createWritable();
+      await writableStream.write(blob);
+      await writableStream.close();
+    } catch (e: any) {
+      return e.message.search('user gesture') === -1 && this.downloadRecording(blob);
+    }
+  };
+  
+  recording = ScreenRecording({
+    processVideo: (blob) => {
+      this.downloadRecording(blob);
+      console.log(blob);
+    },
+    processChunks: (chunk) => {
+      console.log(chunk);
+    },
+    interval: 4000,
+  });
+
+  ngDoCheck() {
+    if(this.recording.getRecordingInProgress() && !this.interval) {
+      this.interval = setInterval(() => this.blinking = !this.blinking, 1000);
+    } else if(!this.recording.getRecordingInProgress() && this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+      this.blinking = false
+    }
+  }
+}
